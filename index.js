bee633318150a4f0e5b2e925721a9d02e8a6a93a
Junior <junior@cashup.com.br>
2023-05-22 21:59:35 -0300

    progress

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+require('dotenv').config()
 const express = require('express');
-
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-}
-);
+app.get('/', async (req, res) => {
+    
+    var uri = `https://www.alura.com.br/api/dashboard/${process.env.ALURA_API_KEY}`;
+
+    const resp = await fetch(uri);
+    const data = await resp.json();
+    const {courseProgresses, guides} = data;
+    res.json(guides).status(200);
+});
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
-}
-);
+});
